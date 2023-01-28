f99e16ec2edcc4ba04a880c0a812cd2c124ed3c5
Vittorio Camisa <vittorio.camisa@gmail.com>
2023-01-28 16:05:36 +0100

    fix: bask correct folder plus curl silent removed

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ VERSION="dev"
 INSTALLATION_PATH="/opt/bask"
 REPO_NAME="bash-kifezza"
 
-
-curl -L -o -s bask.tar.gz https://github.com/ProvoK/${REPO_NAME}/archive/refs/tags/${VERSION}.tar.gz 2>/dev/null
-mkdir -p /tmp/bask
 rm -rf /opt/bask
+
+curl -L -o bask.tar.gz https://github.com/ProvoK/${REPO_NAME}/archive/refs/tags/${VERSION}.tar.gz 2>/dev/null
+mkdir -p /tmp/bask
 mkdir -p /opt/bask
 tar -xvf bask.tar.gz --directory /tmp/bask
 mv /tmp/bask/${REPO_NAME}-${VERSION}/commands ${INSTALLATION_PATH}
